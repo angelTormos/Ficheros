a5598c63a19b93b300de65583bf830f533ec2fe3
Angel Tormos Gual <angtorgua2@alu.edu.gva.es>
2024-04-29 09:06:27 +0200

    Update

---
+++ src/main/java/Ejercicio1.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String[] args) throws IOException {
+        int contador = 0;
+        int opcion;
+        Scanner scanner = new Scanner(System.in);
+        File f = File.listRoots()[0];
+        do {
+        System.out.println("Lista de ficheros y directorios del directorio: ");
+        System.out.println("---------------------------------------------------");
+        for (String e : f.list()) {
+            contador ++;
+                if (f.isFile()) {
+                    System.out.println(contador + ".- " + e + " " + f.length() + " bytes");
+                }
+                if (f.isDirectory()) {
+                    System.out.println(contador + ".- " + e + " <Directorio>");
+                }
+            }
+            System.out.println("Introduce un número");
+            opcion = scanner.nextInt();
+            scanner.nextLine();
+        }while (opcion != 1);
+    }
+}

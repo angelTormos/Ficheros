41dd89e460415d1ba8b89040542c04f5d29c8522
Angel Tormos Gual <angtorgua2@alu.edu.gva.es>
2024-04-30 09:42:26 +0200

    Definitivo

--- src/main/java/Gestion/Ejercicio1.java
+++ src/main/java/Gestion/Ejercicio1.java
@@ -7,25 +7,38 @@ import java.util.Scanner;
 public class Ejercicio1 {
     public static void main(String[] args) throws IOException {
         int contador = 0;
-        int opcion;
+        int opcion = 0;
         Scanner scanner = new Scanner(System.in);
         File f = File.listRoots()[0];
         do {
-        System.out.println("Lista de ficheros y directorios del directorio: ");
+        System.out.println("Lista de ficheros y directorios del directorio: " + f);
         System.out.println("---------------------------------------------------");
         System.out.println(contador + ".- " + "Directorio Padre");
-        for (String e : f.list()) {
+        for (File e : f.listFiles()) {
             contador ++;
-                if (f.isFile()) {
-                    System.out.println(contador + ".- " + e + " " + f.length() + " bytes");
+                if (e.isFile()) {
+                    System.out.println(contador + ".- " + e.getName() + " " + f.length() + " bytes");
                 }
-                if (f.isDirectory()) {
-                    System.out.println(contador + ".- " + e + " <Directorio>");
+                if (e.isDirectory()) {
+                    System.out.println(contador + ".- " + e.getName() + " <Directorio>");
                 }
             }
             System.out.println("Introduce un número");
             opcion = scanner.nextInt();
             scanner.nextLine();
-        }while (opcion != 1);
+            if (opcion == 0){
+                f = f.getParentFile();
+                contador = 0;
+            }else if (opcion != -1){
+                contador = 0;
+                if (opcion >= 0 && opcion <= f.listFiles().length){
+                    if (f.listFiles()[opcion - 1].isDirectory() && f.listFiles()[opcion - 1].canRead()){
+                        f = f.listFiles()[opcion - 1];
+                    } else if (f.listFiles()[opcion - 1].isFile()) {
+                        System.out.println("Esto es un archivo");
+                    }
+                }
+            }
+        }while (opcion != -1);
     }
 }
